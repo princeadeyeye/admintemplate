d7d1d2adc2ac3b7c925a750f2fd25883897432e7
unknown <muizadeyeye@gmail.com>
2019-12-26 11:13:46 +0100

    adding project dependencies

---
+++ admintemplate/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TopBar from './components/TopBar'
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+     {/* <SideBar />
+           <DashhBoard />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ admintemplate/src/components/TopBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TopBar extends Component {
+
+    render() {
+        return (
+        	<div>
+
+        	</div>
+        );
+    }
+}
+
+export default TopBar;
